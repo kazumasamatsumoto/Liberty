59052bb62d78d85c46f77a777f0fefe4cd25d4eb
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-02-01 17:32:04 +0900

    匿名ログイン

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,26 @@ export class LoginPage implements OnInit {
   username = '';
   password = '';
 
-  constructor() { }
+  constructor(private firebaseAuthentication: FirebaseAuthentication) { }
 
   ngOnInit() {
   }
 
   login() {
-    const { username, password } = this;
+    this.firebaseAuthentication.signInAnonymously()
+    // tslint:disable-next-line:only-arrow-functions
+    .catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+
+      if (errorCode === 'auth/operation-not-allowed') {
+        alert('You must enable Anonymous auth in the Firebase Console.');
+      } else {
+        console.error(error);
+      }
+    });
   }
 
 }
