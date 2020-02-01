abca1170ed74e03819ade44e2c05c45a072655fe
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-02-01 17:10:34 +0900

    不要な物削除

--- src/app/qr-create/qr-create.page.ts
+++ src/app/qr-create/qr-create.page.ts
@@ -1,5 +1,4 @@
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
@@ -14,35 +13,9 @@ export class QrCreatePage implements OnInit {
   scannedCode = null;
   elementType: 'url' | 'canvas' | 'img' = 'canvas';
 
-  constructor(private barcodeScanner: BarcodeScanner, private base64ToGallery: Base64ToGallery,
-              private toastCtrl: ToastController) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  scanCode() {
-    this.barcodeScanner.scan().then(
-      barcodeData => {
-        this.scannedCode = barcodeData;
-      }
-    );
-  }
-
-  downloadQR() {
-    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-    const imageData = canvas.toDataURL('image/jpg').toString();
-    console.log('data:', imageData);
-
-    const data = imageData.split(',')[1];
-
-    this.base64ToGallery.base64ToGallery(data,
-      {prefix: '_img', mediaScanner: true })
-      .then(async res => {
-        const toast = await this.toastCtrl.create({
-          header: 'QR Code saved in your Photolibrary'
-        });
-      }, err => console.log('err:', err)
-      );
   }
 
 }
--- src/app/qr-scanner/qr-scanner.page.ts
+++ src/app/qr-scanner/qr-scanner.page.ts
@@ -1,8 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-qr-scanner',
@@ -14,8 +12,7 @@ export class QrScannerPage implements OnInit {
   scannedCode = null;
   elementType: 'url' | 'canvas' | 'img' = 'canvas';
 
-  constructor(private barcodeScanner: BarcodeScanner, private base64ToGallery: Base64ToGallery,
-              private toastCtrl: ToastController) { }
+  constructor(private barcodeScanner: BarcodeScanner) { }
 
   ngOnInit() {
   }
@@ -27,22 +24,5 @@ export class QrScannerPage implements OnInit {
       }
     );
   }
-
-  downloadQR() {
-    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-    const imageData = canvas.toDataURL('image/jpg').toString();
-    console.log('data: ', imageData);
-
-    const data = imageData.split(',')[1];
-
-    this.base64ToGallery.base64ToGallery(data,
-      {prefix: '_img', mediaScanner: true})
-      .then(async res => {
-        const toast = await this.toastCtrl.create({
-          header: 'QR Code saved in your Photolibrary'
-        });
-      }, err => console.log('err', err));
-  }
-
 }
 
